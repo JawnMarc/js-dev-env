2451d67a56c6724882c8f8367098f8f0ea17589c
Jawn Marc <awukum18@gmail.com>
2019-06-21 15:52:42 +0000

    new files; babelrc and webpack.config

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,16 +1,26 @@
-let express = require("express");
-let path = require("path");
-let open = require("open");
+import express from "express";
+import { join } from "path";
+import open from "open";
+import webpack from "webpack";
+import config from "../webpack.config.dev";
 
-let port = 3000;
-let app = express();
+const port = 3000;
+const app = express();
+const compiler = webpack(config);
+
+app.use(
+  require("webpack-dev-middleware")(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+  })
+);
 
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "../src/index.html"));
+  res.sendFile(join(__dirname, "../src/index.html"));
 });
 
 app.listen(port, function(err) {
-  if (err) {
+  if (err) {   
     console.log(err);
   } else {
     open("http://localhost:" + port);
